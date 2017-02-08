53a968f51a645d88e727c87ba9e44ea6ff69a1cf
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-02-08 16:04:39 -0600

    Can read and store config info... Added makefile/testin

--- 6450/osd_p2/p2.c
+++ 6450/osd_p2/p2.c
@@ -1,5 +1,11 @@
+// TODO: try rewriting to allocate memory at runtime as needed, rather than using #defines
 #include <stdio.h>
 #include <string.h>
+
+#define MAX_LINE_LENGTH 1000
+#define CONFIG_START_SIZE 20
+#define MAX_LINE_TOKENS 10
+#define MAX_TOKEN_LENGTH 128
 
 int main(int argc, char *argv[])
 {
@@ -12,44 +18,57 @@ int main(int argc, char *argv[])
 
 	char *filename = argv[1];
 
-	FILE *userFile = fopen(filename, 'r');
+	FILE *userFile = fopen(filename, "r");
 
-	
+	char line[MAX_LINE_LENGTH];		// hold the lines as they're read from the file
+	char *p;						// general use pointer
 
+	char config[CONFIG_START_SIZE][MAX_LINE_TOKENS][MAX_TOKEN_LENGTH];					// the configuration specified in the file
+	char operations[CONFIG_START_SIZE][MAX_LINE_TOKENS][MAX_TOKEN_LENGTH];				// the operations specified in the file
 
+	int configDone = 0;
+	int lineCount = 0;
+	int tokenIndex = 0;
 
-
-
-
-
-    while (fgets(line,1000,stdin) != NULL)
-    {
-		line[strlen(line)-1] = '\0';  /* overlay \n */
-		printf("LINE=:%s:\n",line);
-	if (p = (strchr(line,'#')))
+	while(fgets(line, MAX_LINE_LENGTH, userFile) != NULL)
 	{
-	    *p = '\0';
+		line[strlen(line) - 1] = '\0';
+
+		if(p = (strchr(line, '#')))
+			*p = '\0';
+
+		if(line[0] != 'v')
+		{
+			configDone = 1; 
+			lineCount = 0;
+		}
+
+		// TODO: if I go over the starting line count, need to get more space
+
+		if(!configDone)
+		{
+			if(p = strtok(line, " \t"))
+			{
+				strcpy(config[lineCount][0], p);
+
+				for(tokenIndex = 1; tokenIndex < MAX_LINE_TOKENS; tokenIndex++)
+				{
+					if(p = strtok(NULL, " \t"))
+						strcpy(config[lineCount][tokenIndex], p);
+					else
+						break;
+				}
+			}
+		}
+		else
+		{
+
+		}
+
+		lineCount++;
 	}
-        for (i=0; i < 16; i++)
-        {
-            args[i][0] = '\0';
-        }
-	if (p = strtok(line," \t"))
-	    strcpy(args[0],p);
-        printf("arg 0  :%s:\n",args[0]);
-        for (i=1; i < 16; i++)  // start at 1
-        {
-            if (p = strtok(NULL," \t"))
-            {
-                strcpy(args[i],p);
-                printf("arg %d  :%s:\n",i,args[i]);
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
+
+	fclose(userFile);
 
 	return 0;
 }
