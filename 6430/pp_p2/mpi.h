0d8ba4dda5fbadaa8c0602473f77c3f7bce382e7
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-02-19 15:44:34 -0600

    pp: decided to use the sample_mpi.h

--- 6430/pp_p2/mpi.h
+++ 6430/pp_p2/mpi.h
@@ -1,9 +1,68 @@
-MPI_Comm_rank 	
-MPI_Comm_size 	
-MPI_Finalize 	
-MPI_Init 	
-MPI_Send (just wrapper for Ssend)
-MPI_Ssend
+// based on sample header provided (from mpich?)
+// currently no changes
+typedef int MPI_Datatype;
+#define MPI_CHAR           ((MPI_Datatype)0x4c000101)
+#define MPI_INT            ((MPI_Datatype)0x4c000405)
+
+/* Communicators */
+typedef int MPI_Comm;
+#define MPI_COMM_WORLD ((MPI_Comm)0x44000000)
+
+/* MPI request opjects */
+typedef int MPI_Request;
+
+typedef int MPI_Aint;
+
+/* Collective operations */
+typedef int MPI_Op;
+
+#define MPI_MAX     (MPI_Op)(0x58000001)
+#define MPI_MIN     (MPI_Op)(0x58000002)
+#define MPI_SUM     (MPI_Op)(0x58000003)
+#define MPI_PROD    (MPI_Op)(0x58000004)
+#define MPI_LAND    (MPI_Op)(0x58000005)
+#define MPI_BAND    (MPI_Op)(0x58000006)
+#define MPI_LOR     (MPI_Op)(0x58000007)
+#define MPI_BOR     (MPI_Op)(0x58000008)
+#define MPI_LXOR    (MPI_Op)(0x58000009)
+#define MPI_BXOR    (MPI_Op)(0x5800000a)
+#define MPI_MINLOC  (MPI_Op)(0x5800000b)
+#define MPI_MAXLOC  (MPI_Op)(0x5800000c)
+#define MPI_REPLACE (MPI_Op)(0x5800000d)
+
+typedef struct MPI_Status {
+    int count;
+    int cancelled;
+    int MPI_SOURCE;
+    int MPI_TAG;
+    int MPI_ERROR;
+} MPI_Status;
+
+/* RMA and Windows */
+typedef int MPI_Win;
+#define MPI_WIN_NULL ((MPI_Win)0x20000000)
+
+/* for info */
+typedef int MPI_Info;
+#define MPI_INFO_NULL         ((MPI_Info)0x1c000000)
+#define MPI_MAX_INFO_KEY       255
+#define MPI_MAX_INFO_VAL      1024
+
+/* MPI's error classes */
+#define MPI_SUCCESS          0      /* Successful return code */
+/* Communication argument parameters */
+#define MPI_ERR_BUFFER       1      /* Invalid buffer pointer */
+#define MPI_ERR_COUNT        2      /* Invalid count argument */
+#define MPI_ERR_TYPE         3      /* Invalid datatype argument */
+#define MPI_ERR_TAG          4      /* Invalid tag argument */
+#define MPI_ERR_COMM         5      /* Invalid communicator */
+#define MPI_ERR_RANK         6      /* Invalid rank */
+#define MPI_ERR_ROOT         7      /* Invalid root */
+#define MPI_ERR_TRUNCATE    14      /* Message truncated on receive */
+
+
+/* We require that the C compiler support prototypes */
+/* Begin Prototypes */
 int MPI_Init(int *, char ***);
 int MPI_Finalize(void);
 int MPI_Abort(MPI_Comm, int);
@@ -31,6 +90,4 @@ int MPI_Get(void *, int, MPI_Datatype, int, MPI_Aint, int, MPI_Datatype, MPI_Win
 int MPI_Put(void *, int, MPI_Datatype, int, MPI_Aint, int, MPI_Datatype, MPI_Win);
 int MPI_Win_create(void *, MPI_Aint, int, MPI_Info, MPI_Comm, MPI_Win *);
 int MPI_Win_lock(int, int, int, MPI_Win);
-
-int MPI_Barrier(MPI_Comm );
-int MPI_Recv(void*, int, MPI_Datatype, int, int, MPI_Comm, MPI_Status *);
+int MPI_Win_unlock(int, MPI_Win);
