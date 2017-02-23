545287063d8ece1761bc586ca888f4c7485de723
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-02-23 17:33:46 -0600

    pp: pulled info from environment in MPI_Init

--- 6430/pp_p2/libpp.c
+++ 6430/pp_p2/libpp.c
@@ -1,10 +1,40 @@
 #include "mpi.h"
+#include <stdlib.h>
+#include <string.h>
+
+static int MPI_World_rank;
+static int MPI_World_size;
+static char *MPI_Control_host;		// TODO: free in finalize
+static int MPI_Control_port;
+static char *MPI_My_host;
+static int MPI_My_port;
 
 int MPI_Init(int *argc, char ***argv)
 {
-	// attempt to connect to the control process
+	// get rank, size, host, port from environment
+	MPI_World_rank = atoi(getenv("PP_MPI_RANK"));
+	MPI_World_size = atoi(getenv("PP_MPI_SIZE"));
 
-	return 0;
+	char *p;
+	char *tempString = malloc((strlen(getenv("PP_MPI_HOST_PORT")) + 1) * sizeof(char));
+	strncpy(tempString, getenv("PP_MPI_HOST_PORT"), strlen(getenv("PP_MPI_HOST_PORT")) + 1);
+
+	if(p = strtok(tempString, ":"))
+	{
+		MPI_Control_host = malloc((strlen(p) + 1) * sizeof(char));
+		strncpy(MPI_Control_host, p, strlen(p) + 1);
+
+		if(p = strtok(NULL, " "))
+		{
+			MPI_Control_port = atoi(p);
+		}
+	}
+
+	// get an accept/listen socket and figure out its port
+
+	// connect to ppexec and tell it my host/port
+
+	return MPI_SUCCESS;
 }
 
 int MPI_Finalize(void)
--- 6430/pp_p2/officialt1.c
+++ 6430/pp_p2/officialt1.c
@@ -10,6 +10,12 @@ int main(int argc, char *argv[])
     char hostname[128];
 
     rc = MPI_Init(NULL,NULL);
+    rc = MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+    rc = MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
-	return 0;
+    gethostname(hostname,128);
+
+    printf("%d:  size %d  onhost %s\n",myrank,numranks,hostname);
+
+    MPI_Finalize();
 }
