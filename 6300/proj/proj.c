7a425f1993a78464e1ed125ab1eaa795cfc5bfea
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-03-07 14:11:37 -0600

    net: open/close files

--- 6300/proj/proj.c
+++ 6300/proj/proj.c
@@ -4,14 +4,32 @@
 #include <string.h>
 
 void ErrorCheck(int val, char *str);
+int GetInfoFromFiles(FILE *mapFile, FILE *networkGraphFile, FILE *taskGraphFile);
 
 int main(int argc, char *argv[])
 {
 	// get the map file, network graph, and task graph from the command line
+	if(argc < 4)
+	{
+		puts("Usage: proj mapFile, networkGraph, taskGraph");
+		return 1;
+	}
 
 	// open the files
+	FILE *mapFile = fopen(argv[1], "r");
+	FILE *networkGraphFile = fopen(argv[2], "r");
+	FILE *taskGraphFile = fopen(argv[3], "r");
 
 	// get relevant information from each file
+	if(GetInfoFromFiles(mapFile, networkGraphFile, taskGraphFile) != 0)
+	{
+		puts("Error in setup files");
+		return 1;
+	}
+
+	fclose(mapFile);
+	fclose(networkGraphFile);
+	fclose(taskGraphFile);
 
 	return 0;
 }
@@ -23,4 +41,11 @@ void ErrorCheck(int val, char *str)
 		printf("%s : %d : %s\n", str, val, strerror(errno));
 		exit(1);
 	}
+}
+
+int GetInfoFromFiles(FILE *mapFile, FILE *networkGraphFile, FILE *taskGraphFile)
+{
+
+
+	return 0;
 }
