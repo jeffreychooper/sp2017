1ae56539fbe0d20deae590c6eb24d3eef525b1d9
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-03-07 13:38:17 -0600

    net: added makefile and started implementation

---
+++ 6300/proj/proj.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+	// get the map file, network graph, and task graph from the command line
+
+	// open the files
+
+	// get relevant information from each file
+
+	return 0;
+}
