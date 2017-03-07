521787373d8e687f3227fdbc2de2b6adab5b040d
Jeffrey Hooper <jeffreychooper@gmail.com>
2017-03-07 13:53:07 -0600

    net: added ErrorCheck function

--- 6300/proj/proj.c
+++ 6300/proj/proj.c
@@ -1,10 +1,22 @@
+void ErrorCheck(int val, char *str);
+
 int main(int argc, char *argv[])
 {
 	// get the map file, network graph, and task graph from the command line
+	if(argc < 
 
 	// open the files
 
 	// get relevant information from each file
 
 	return 0;
+}
+
+void ErrorCheck(int val, char *str)
+{
+	if(val < 0)
+	{
+		printf("%s : %d : %s\n", str, val, strerror(errno));
+		exit(1);
+	}
 }
